2054b26a374f5451cb0e508c432ced8bd1b6a2fa
arisa <arisankochira10@gmail.com>
2019-12-03 08:17:18 +0900

    indexページの作成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,8 @@
-  class PostsController < ApplicationController
+class PostsController < ApplicationController
+  def index
+    @posts = Post.order(created_at: :desc)
+  end
+
 
   def new
     @post = Post.new # フォーム用の空のインスタンスを生成する。
@@ -7,12 +11,20 @@
   def create
     @post = Post.new(post_params) # ストロングパラメータを引数に
     @post.save # saveをしてデータベースに保存する。
-    redirect_to @post # showページにリダイレクト
+    if @post.save
+      redirect_to @post, notice: "投稿を登録しました。"
+    else
+      render :new
+    end
+  end
+
+  def show
+    @post = Post.find(params[:id])
   end
 
   private
 
   def post_params # ストロングパラメータを定義する
-    params.require(:post).permit(:caption, :image)
+    params.require(:post).permit(:caption, :new_image)
   end
-  end
+end

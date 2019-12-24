a39951d50761295bdf2fa1f6fe8c369f244ecbbc
arisa <arisankochira10@gmail.com>
2019-12-24 12:35:51 +0900

    詳細で

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -17,6 +17,6 @@ class CommentsController < ApplicationController
   private
 
   def comment_params
-    params.require(:comment).permit(:body, :post_id, :user_id)
+    params.require(:comment).permit(:body, :post_id)
   end
 end
